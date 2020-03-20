83e72a265589889f22afcf5666c545198c693624
Lee kiseop <dlrltjq1004@gmail.com>
2020-03-20 16:46:28 +0900

    Updates

---
+++ precache-manifest.0387add76b55a73c5520cce92e40f29d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b075382067fadf4396cc32c1412eb755",
+    "url": "/antiredux/index.html"
+  },
+  {
+    "revision": "e30c48df6881987b797f",
+    "url": "/antiredux/static/js/2.26fcfab2.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/antiredux/static/js/2.26fcfab2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "832fe3c95739b704e043",
+    "url": "/antiredux/static/js/main.45a85680.chunk.js"
+  },
+  {
+    "revision": "1443bdc22a5b83225512",
+    "url": "/antiredux/static/js/runtime-main.a611c20d.js"
+  }
+]);
